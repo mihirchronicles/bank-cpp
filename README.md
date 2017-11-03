801d70d6b305ac3254249b4be6ce8fc1dcf04c49
MIHIR PATEL <mihirptl89@gmail.com>
2017-11-03 04:11:21 -0500

    banking in c++

---
+++ README.md
@@ -0,0 +1,11 @@
+#Bank Program Written in C++
+
+##Basic Operations:
+
+1. Create Account
+2. Show Account Details
+3. Modify Balances in Accounts (Debit/Credit)
+4. Show Report in a tabular format
+5. Show Current Balance 
+6. Show Account Number
+7. Show Account Type (Checking or Savings)
---
+++ bank.cpp
@@ -0,0 +1,20 @@
+// HEADER FILE
+
+#include<iostream>
+using namespace std;
+
+class account
+{
+
+public:
+	void create_account();	//function to get data from user
+	void show_account() const;	//function to show data on screen
+	void modify();	//functiodn to add new data
+	void dep(int);	//function to accept amount and add to balance amount
+	void draw(int);	//function to accept amount and subtract from balance amount
+	void report() const;	//function to show data in tabular format
+	int retacno() const;	//function to return account number
+	int retdeposit() const;	//function to return balance amount
+	char rettype() const;	//function to return type of account
+}; 
+
